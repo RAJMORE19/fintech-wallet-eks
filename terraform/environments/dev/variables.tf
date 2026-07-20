45da8b65e792e0fd29eddd9d98bbd6205f42de0b
Raj More <167281073+RAJMORE19@users.noreply.github.com>
2026-07-20 17:44:50 +0530

    Update variables.tf

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -1,5 +1,9 @@
+########################################
+# Base Project Configurations
+########################################
+
 variable "project_name" {
-  description = "Project name"
+  description = "Project name used for resource naming"
   type        = string
 }
 
@@ -13,27 +17,68 @@ variable "environment" {
   }
 }
 
-variable "vpc_cidr" {
-  description = "VPC CIDR Block"
+########################################
+# Network Infrastructure Inputs
+########################################
+
+variable "vpc_id" {
+  description = "The ID of the VPC where EKS will be deployed"
   type        = string
 }
 
-variable "availability_zones" {
-  description = "Availability Zones"
+variable "private_subnet_ids" {
+  description = "List of private subnet IDs for EKS control plane and nodes"
   type        = list(string)
 }
 
-variable "public_subnet_cidrs" {
-  description = "Public Subnet CIDRs"
-  type        = list(string)
+################ #######################
+# Identity & Access Management (IAM)
+################ #######################
+
+variable "cluster_role_arn" {
+  description = "IAM Role ARN for the EKS Cluster control plane"
+  type        = string
 }
 
-variable "private_subnet_cidrs" {
-  description = "Private Subnet CIDRs"
-  type        = list(string)
+variable "worker_node_role_arn" {
+  description = "IAM Role ARN for the EKS Worker Node groups"
+  type        = string
 }
 
-variable "database_subnet_cidrs" {
-  description = "Database Subnet CIDRs"
+################ #######################
+# EKS Cluster Configurations
+################ #######################
+
+variable "cluster_version" {
+  description = "Kubernetes minor version to use for the EKS cluster"
+  type        = string
+  default     = "1.31"
+}
+
+################ #######################
+# Worker Node Capacity Settings
+################ #######################
+
+variable "node_instance_types" {
+  description = "Instance types used for EKS worker nodes"
   type        = list(string)
+  default     = ["t3.medium"]
+}
+
+variable "node_scaling_desired" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "node_scaling_max" {
+  description = "Maximum number of worker nodes for scaling up"
+  type        = number
+  default     = 4
+}
+
+variable "node_scaling_min" {
+  description = "Minimum number of worker nodes for scaling down"
+  type        = number
+  default     = 1
 }
