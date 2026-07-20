ac19efa891972bd36d3483792769f5b7865fbe90
Raj More <167281073+RAJMORE19@users.noreply.github.com>
2026-07-20 17:43:05 +0530

    Update variables.tf

---
+++ terraform/modules/eks/variables.tf
@@ -0,0 +1,54 @@
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment (dev, staging, prod)"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where EKS will be created"
+  type        = string
+}
+
+variable "private_subnet_ids" {
+  description = "Private subnet IDs for EKS worker nodes"
+  type        = list(string)
+}
+
+variable "cluster_role_arn" {
+  description = "IAM Role ARN for the EKS Cluster"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "EKS Kubernetes version"
+  type        = string
+  default     = "1.33"
+}
+
+variable "node_instance_type" {
+  description = "EC2 instance type for worker nodes"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "desired_size" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "min_size" {
+  description = "Minimum worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "max_size" {
+  description = "Maximum worker nodes"
+  type        = number
+  default     = 5
+}
